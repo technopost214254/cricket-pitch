ee530dc5f8d4dfa0c5dcc97bca4166a099d0dc8d
vikas vishwakarma <vikas.vishwakarma749@webkul.com>
2025-01-30 12:48:49 +0530

    cricket pitch

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Ground from './Components/Ground';
+import './App.css';
+
+function App() {
+  return (
+    <Ground />
+  );
+}
+
+export default App;
---
+++ src/Components/Ground.js
@@ -0,0 +1,17 @@
+function Ground() {
+    return (
+      <div className="ground">
+        <div className="boundary">
+            <div className="yard">
+                <div className="pitch">
+                    <div className="crease-bat"></div>
+                    <div className="crease-bol"></div>
+                </div>
+            </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Ground;
+  
